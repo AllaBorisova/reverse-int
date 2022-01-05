9137db0d0b1aa4c2d9503075f230118cfdef4936
Alla Borisova <alla.borisova27@gmail.com>
2022-01-05 18:14:16 +0500

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+	let str = String(Math.abs(n));
+	let strReverse = '';
+	for (let i = str.length-1; i >= 0; i--){
+		strReverse += str[i];
+	}
+	return Number(strReverse);
 }
